aff62d554ce8838e37cd906972bbe1d0f3a7d224
Kei-Shiro <razafindralambonahary04@gmail.com>
2025-07-07 11:40:16 +0300

    modifie mandeha

--- ws/index.php
+++ ws/index.php
@@ -16,18 +16,18 @@ Flight::route('GET /etudiants/@id', function($id) {
 });
 
 Flight::route('POST /etudiants', function() {
-    $data = Flight::request()->data;
     $db = getDB();
     $stmt = $db->prepare("INSERT INTO s4_bank_etudiant (nom, prenom, email, age) VALUES (?, ?, ?, ?)");
-    $stmt->execute([$data->nom, $data->prenom, $data->email, $data->age]);
+    $stmt->execute([$_POST['nom'], $_POST['prenom'], $_POST['email'], $_POST['age']]);
     Flight::json(['message' => 'Étudiant ajouté', 'id' => $db->lastInsertId()]);
 });
 
 Flight::route('PUT /etudiants/@id', function($id) {
-    $data = Flight::request()->data;
+    // Pour les requêtes PUT, on doit parser manuellement les données
+    parse_str(file_get_contents('php://input'), $data);
     $db = getDB();
     $stmt = $db->prepare("UPDATE s4_bank_etudiant SET nom = ?, prenom = ?, email = ?, age = ? WHERE id = ?");
-    $stmt->execute([$data->nom, $data->prenom, $data->email, $data->age, $id]);
+    $stmt->execute([$data['nom'], $data['prenom'], $data['email'], $data['age'], $id]);
     Flight::json(['message' => 'Étudiant modifié']);
 });
 
